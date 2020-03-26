2067d5535b961310d5e79b3aa4254cfc32e6ec53
shasha <shashasi8023@163.com>
2020-03-26 19:18:44 +0800

    [shasha] refactor: test name

--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class LockerTest {
   @Test
-  void should_return_ticket_when_locker_available_capacity_is_max() {
+  void should_return_ticket_when_locker_available_capacity_is_not_zero() {
     //Given
     Parcel parcel = new Parcel();
     Locker locker = new Locker(20);
@@ -16,7 +16,7 @@ public class LockerTest {
   }
 
   @Test
-  void should_locker_available_capacity_decrease_1_when_locker_available_capacity_is_max() {
+  void should_locker_available_capacity_decrease_1_when_locker_available_capacity_is_not_zero() {
     //Given
     Parcel parcel = new Parcel();
     Locker locker = new Locker(20);
@@ -26,7 +26,6 @@ public class LockerTest {
     //Then
     Assertions.assertEquals(19, availableCapacity);
   }
-
 
 
 }
