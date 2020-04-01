a2c26b8196429e83267a1da64645a8e2d5397316
HaoYu Luo <haoyu.luo@thoughtworks.com>
2020-04-01 21:04:01 +0800

    refactor locker

--- src/main/java/cn/xpbootcamp/gilded_rose/model/Locker.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/model/Locker.java
@@ -7,46 +7,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Locker {
-  private int capacity;
-  private int availableCapacity;
-  private Map<Ticket, Parcel> storeInformation;
+    private int capacity;
+    private int availableCapacity;
+    private Map<Ticket, Parcel> storeInformation;
 
-  private Locker(int capacity, int availableCapacity) {
-    this.capacity = capacity;
-    this.availableCapacity = availableCapacity;
-    this.storeInformation = new HashMap<>();
-  }
-
-  public static Locker constructLocker(int capacity, int availableCapacity) {
-    if (availableCapacity >=0 && availableCapacity <= capacity) {
-      return new Locker(capacity, availableCapacity);
+    public Locker(int capacity) {
+        this.capacity = capacity;
+        this.availableCapacity = capacity;
+        this.storeInformation = new HashMap<>();
     }
-    throw new RuntimeException("argument is illegal");
-  }
 
-  public Ticket store(Parcel parcel) {
-    if (this.availableCapacity <= 0) {
-      throw new StoreParcelException("locker is full");
+
+    public Ticket store(Parcel parcel) {
+        if (this.availableCapacity <= 0) {
+            throw new StoreParcelException("locker is full");
+        }
+        this.availableCapacity = this.availableCapacity - 1;
+        Ticket ticket = new Ticket();
+        storeInformation.put(ticket, parcel);
+        return ticket;
     }
-    this.availableCapacity = this.availableCapacity - 1;
-    Ticket ticket = new Ticket();
-    storeInformation.put(ticket, parcel);
-    return ticket;
-  }
 
-  public int getAvailableCapacity() {
-    return availableCapacity;
-  }
-
-  public Parcel collect(Ticket ticket) {
-    for (Ticket t : storeInformation.keySet()) {
-      if (t.getId().equals(ticket.getId())) {
-        this.availableCapacity = this.availableCapacity + 1;
-        Parcel collectedParcel = storeInformation.get(t);
-        storeInformation.remove(t);
-        return collectedParcel;
-      }
+    public int getAvailableCapacity() {
+        return availableCapacity;
     }
-    throw new CollectParcelException("ticket is illegal");
-  }
+
+    public Parcel collect(Ticket ticket) {
+        for (Ticket t : storeInformation.keySet()) {
+            if (t.getId().equals(ticket.getId())) {
+                this.availableCapacity = this.availableCapacity + 1;
+                Parcel collectedParcel = storeInformation.get(t);
+                storeInformation.remove(t);
+                return collectedParcel;
+            }
+        }
+        throw new CollectParcelException("ticket is illegal");
+    }
 }
--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -9,75 +9,75 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LockerTest {
-  @Test
-  void should_return_ticket_when_locker_available_capacity_is_not_zero() {
-    //Given
-    Parcel parcel = new Parcel();
-    Locker locker = Locker.constructLocker(20, 20);
-    //When
-    Ticket ticket = locker.store(parcel);
-    //Then
-    Assertions.assertNotNull(ticket);
-  }
+    @Test
+    void should_return_ticket_when_locker_available_capacity_is_not_zero() {
+        //Given
+        Parcel parcel = new Parcel();
+        Locker locker = new Locker(20);
+        //When
+        Ticket ticket = locker.store(parcel);
+        //Then
+        Assertions.assertNotNull(ticket);
+    }
 
-  @Test
-  void should_locker_available_capacity_decrease_1_when_locker_available_capacity_is_not_zero() {
-    //Given
-    Parcel parcel = new Parcel();
-    Locker locker = Locker.constructLocker(20, 20);
-    //When
-    locker.store(parcel);
-    int availableCapacity = locker.getAvailableCapacity();
-    //Then
-    Assertions.assertEquals(19, availableCapacity);
-  }
+    @Test
+    void should_locker_available_capacity_decrease_1_when_locker_available_capacity_is_not_zero() {
+        //Given
+        Parcel parcel = new Parcel();
+        Locker locker = new Locker(20);
+        //When
+        locker.store(parcel);
+        int availableCapacity = locker.getAvailableCapacity();
+        //Then
+        Assertions.assertEquals(19, availableCapacity);
+    }
 
-  @Test
-  void should_throw_exception_when_store_parcel_and_locker_available_capacity_is_zero() {
-    //Given
-    Parcel parcel = new Parcel();
-    Locker locker = Locker.constructLocker(20, 0);
-    //When Then
-    Assertions.assertThrows(StoreParcelException.class, () -> locker.store(parcel));
-  }
+    @Test
+    void should_throw_exception_when_store_parcel_and_locker_available_capacity_is_zero() {
+        //Given
+        Parcel parcel = new Parcel();
+        Locker locker = new Locker(0);
+        //When Then
+        Assertions.assertThrows(StoreParcelException.class, () -> locker.store(parcel));
+    }
 
-  @Test
-  void should_collect_parcel_when_ticket_is_legal() {
-    //Given
-    Locker locker = Locker.constructLocker(20, 10);
-    Parcel parcel = new Parcel();
-    Ticket ticket = locker.store(parcel);
-    //When
-    Parcel collectedParcel = locker.collect(ticket);
-    //Then
-    Assertions.assertEquals(parcel, collectedParcel);
-  }
+    @Test
+    void should_collect_parcel_when_ticket_is_legal() {
+        //Given
+        Locker locker = new Locker(10);
+        Parcel parcel = new Parcel();
+        Ticket ticket = locker.store(parcel);
+        //When
+        Parcel collectedParcel = locker.collect(ticket);
+        //Then
+        Assertions.assertEquals(parcel, collectedParcel);
+    }
 
-  @Test
-  void should_locker_available_capacity_increase_1_when_ticket_is_legal() {
-    //Given
-    Locker locker = Locker.constructLocker(20, 10);
-    Parcel parcel = new Parcel();
-    Ticket ticket = locker.store(parcel);
-    int availableCapacity = locker.getAvailableCapacity();
+    @Test
+    void should_locker_available_capacity_increase_1_when_ticket_is_legal() {
+        //Given
+        Locker locker = new Locker(10);
+        Parcel parcel = new Parcel();
+        Ticket ticket = locker.store(parcel);
+        int availableCapacity = locker.getAvailableCapacity();
 
-    Assertions.assertEquals(9, availableCapacity);
-    //When
-    locker.collect(ticket);
-    int newAvailableCapacity = locker.getAvailableCapacity();
-    //Then
-    Assertions.assertEquals(10, newAvailableCapacity);
-  }
+        Assertions.assertEquals(9, availableCapacity);
+        //When
+        locker.collect(ticket);
+        int newAvailableCapacity = locker.getAvailableCapacity();
+        //Then
+        Assertions.assertEquals(10, newAvailableCapacity);
+    }
 
-  @Test
-  void should_throw_exception_when_ticket_is_illegal() {
-    //Given
-    Locker locker = Locker.constructLocker(20, 10);
-    Parcel parcel = new Parcel();
-    Ticket ticket = locker.store(parcel);
-    //When
-    locker.collect(ticket);
-    //Then
-    Assertions.assertThrows(CollectParcelException.class, () -> locker.collect(ticket));
-  }
+    @Test
+    void should_throw_exception_when_ticket_is_illegal() {
+        //Given
+        Locker locker = new Locker(10);
+        Parcel parcel = new Parcel();
+        Ticket ticket = locker.store(parcel);
+        //When
+        locker.collect(ticket);
+        //Then
+        Assertions.assertThrows(CollectParcelException.class, () -> locker.collect(ticket));
+    }
 }
