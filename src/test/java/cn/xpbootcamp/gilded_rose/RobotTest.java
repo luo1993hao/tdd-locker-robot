97aeada2aba684ab333dd36aeda183550e3993df
shasha <shashasi8023@163.com>
2020-04-02 21:03:46 +0800

    [shasha] feat: store parcel test

--- src/test/java/cn/xpbootcamp/gilded_rose/RobotTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/RobotTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 
 public class RobotTest {
     @Test
-    void should_return_ticket_when_locker_capacity_is_10_given_parcel_and_robot() {
+    void should_return_ticket_when_store_parcel_given_parcel_locker_with_capacity_10_and_robot() {
         //given
         Robot robot = new Robot(Lists.newArrayList(new Locker(10)));
         Parcel beStoreParcel = new Parcel();
@@ -37,7 +37,7 @@ public class RobotTest {
     }
 
     @Test
-    void should_store_in_first_one_and_return_ticket_when_store_parcel_given_parcel_and_both_of_two_locker_capacity_is_10_robot() {
+    void should_store_in_first_one_and_return_ticket_when_store_parcel_given_parcel_and_both_of_two_locker_capacity_are_10_robot() {
         //given
         Robot robot = new Robot(Lists.newArrayList(new Locker(10), new Locker(10)));
         Parcel beStoreParcel = new Parcel();
@@ -53,7 +53,7 @@ public class RobotTest {
     }
 
     @Test
-    void should_throw_exception_when_store_parcel_given_parcel_and_two_locker_capacity_is_0_robot() {
+    void should_throw_exception_when_store_parcel_given_parcel_and_two_locker_capacity_are_0_robot() {
         //given
         Robot robot = new Robot(Lists.newArrayList(new Locker(0), new Locker(0)));
         Parcel beStoreParcel = new Parcel();
@@ -62,18 +62,31 @@ public class RobotTest {
     }
 
     @Test
-    void should_return_parcel_when_collect_parcel_given_valid_ticket_and_robot() {
+    void should_throw_exception_when_store_parcel_given_parcel_and_one_locker_capacity_is_0_another_capacity_is_1_robot() {
         //given
-        Robot robot = new Robot(Lists.newArrayList(new Locker(0), new Locker(10)));
+        Robot robot = new Robot(Lists.newArrayList(new Locker(1), new Locker(0)));
         Parcel beStoreParcel = new Parcel();
-        Ticket ticket = robot.store(beStoreParcel);
+        Parcel failedParcel = new Parcel();
         //when
-        Parcel collectParcel = robot.collect(ticket);
+        robot.store(beStoreParcel);
         //then
-        Assertions.assertEquals(beStoreParcel, collectParcel);
+        Locker firstLocker = robot.getLockers().get(0);
+        Assertions.assertEquals(0, firstLocker.getAvailableCapacity());
+        Assertions.assertThrows(StoreParcelException.class, () -> robot.store(failedParcel));
     }
 
-    //TODO 有没有有没有必要分开测1个robot以及2个robot
+    @Test
+      void should_return_parcel_when_collect_parcel_given_valid_ticket_and_robot() {
+          //given
+          Robot robot = new Robot(Lists.newArrayList(new Locker(0), new Locker(10)));
+          Parcel beStoreParcel = new Parcel();
+          Ticket ticket = robot.store(beStoreParcel);
+          //when
+          Parcel collectParcel = robot.collect(ticket);
+          //then
+          Assertions.assertEquals(beStoreParcel, collectParcel);
+      }
+
     @Test
     void should_throw_exception_when_collect_parcel_given_invalided_ticket_and_robot() {
         //given
